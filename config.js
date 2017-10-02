b7c9f914eb93b4df0f14378ea0ef46d0a51366eb
lorashopova <lshopowa@yahoo.com>
2017-10-02 11:58:35 +0300

    config added
    config added

---
+++ config.js
@@ -0,0 +1,28 @@
+SystemJS.config({
+    'transpiler': 'plugin-babel',
+    'map': {
+        'plugin-babel': './node_modules/systemjs-plugin-babel/plugin-babel.js',
+        'systemjs-babel-build': './node_modules/systemjs-plugin-babel/systemjs-babel-browser.js',
+
+        'jquery': './node_modules/jquery/dist/jquery.js',
+        'popper': './node_modules/popper.js/dist/popper.js',
+        'bootstrap': './node_modules/bootstrap/dist/js/bootstrap.js',
+        'handlebars': './node_modules/handlebars/dist/handlebars.js',
+        'sammy': './node_modules/sammy/lib/sammy.js',
+        'toastr': './node_modules/toastr/toastr.js',
+        'moment': './node_modules/moment/moment.js',
+        
+        'app': './scripts/app.js',
+        'templates': './scripts/utils/template.js',
+        'requester': './scripts/utils/requester.js',
+        'constants': './scripts/utils/constants.js',
+        'pagination': './scripts/utils/pagination.js',        
+        'userModel': './scripts/requests/authentication-requests.js',
+        'appModel': './scripts/requests/app-requests.js',
+        'userController': './scripts/controllers/user-controller.js',
+        'appController': './scripts/controllers/app-controller.js'
+
+    }
+});
+
+System.import('app');
