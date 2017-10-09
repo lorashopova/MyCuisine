342bf7b35885e2cb16c5ed9bd9f9ddb8940a0e15
lorashopova <lshopowa@yahoo.com>
2017-10-09 12:30:52 +0300

    user validations added

--- scripts/controllers/user-controller.js
+++ scripts/controllers/user-controller.js
@@ -3,7 +3,9 @@ import toastr from 'toastr';
 import { templates } from 'templates';
 import { userModel } from 'userModel';
 import { appModel } from 'appModel';
-// import { Validator } from 'validators';
+
+const MIN_LENGTH = 3;
+const MAX_LENGTH = 20;
 
 const userController = (function() {
     class UserController {
@@ -12,18 +14,42 @@ const userController = (function() {
             this.userModel = userModel;
         }
 
-        registerAction(selector, name, username, email, password) {
+        registerAction(selector, name, username, email, password, confirmPassword) {
             $(selector).empty();
             $('#carouselIndicators').addClass('hidden');
+            $('.slider-shadow').addClass('hidden');
             $('.footer-aside').removeClass('hidden');
             $('.home-view').addClass('hidden');
+
+            if(username.length < MIN_LENGTH || username.length > MAX_LENGTH){
+                toastr.error('Username must be between 3 and 20 symbols');
+                return;
+            }
+            if(password < MIN_LENGTH || password.length > MAX_LENGTH){
+                toastr.error('Password must be between 3 and 20 symbols');
+                return;
+            }
+            if(/^[a-zA-Z0-9]+$/.test(username)){
+                toastr.error('Username must start with a letter and contains alphanumetrical symbols only');
+                return;
+            }
+            if(/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/.test(password)){
+                toastr.error('Password must be between 6 and 16 symbols, contain at least one digit and one special character');
+                return;
+            }
+
+            if(password !== confirmPassword){
+                toastr.error('Please confirm correctly password');
+                return;
+            }
+
             userModel.register(name, username, email, password).then((userInfo) => {
                 userModel.saveSession(userInfo);
                 toastr.success('User registration successful!');
                 this.viewChangesAuth();
                 location.hash = '#/home';
-            }).catch((err) => {
-                toastr.error();
+            }).catch((error) => {
+                toastr.error('Invalid username or password!');
             });
         }
 
@@ -32,6 +58,7 @@ const userController = (function() {
             let resultComments; 
             $(selector).empty();
             $('#carouselIndicators').addClass('hidden');
+            $('.slider-shadow').addClass('hidden');
             $('#content-header').removeClass('hidden');
             $('header').css('backgroundImage', 'url("../images/header-bg.png")');
             $('header').css('height', '250px');
@@ -62,20 +89,22 @@ const userController = (function() {
                     $('.list-comments').html(template(resultComments));
                 });
             }).catch((error) => {
-                toastr.error();
+                toastr.error('');
             });
         }
 
         loginAction(selector, username, password) {
             $(selector).empty();
             $('#carouselIndicators').addClass('hidden');
+            $('.slider-shadow').addClass('hidden');
             userModel.login(username, password).then((userInfo) => {
                 userModel.saveSession(userInfo);
                 toastr.success('User login successful!');
                 this.viewChangesAuth();
                 location.hash = '#/home';
-            }).catch((err) => {
-                toastr.error();
+            }).catch((error) => {
+                toastr.error('Invalid username or password!');
+                location.hash = '#/home';
             });
         }
 
